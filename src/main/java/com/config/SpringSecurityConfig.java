46377474a9a02e8ca99f82e507762ed994893593
Sachin Kumar <sjun1992@gmail.com>
2018-06-19 19:59:02 +0530

    Security code is workingas expected

--- src/main/java/com/config/SpringSecurityConfig.java
+++ src/main/java/com/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -20,14 +21,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-		// 1. In memory Authentication
-
-		// auth.inMemoryAuthentication().withUser("sachin").password("{noop}password").roles("USER");
-		// auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("ADMIN");
-		// auth.inMemoryAuthentication().withUser("dba").password("{noop}password").roles("MANAGER");
-
-		// 2. jdbc authentication
 
 		auth.jdbcAuthentication().dataSource(dataSource)
 				.usersByUsernameQuery(
@@ -40,20 +33,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		// 1. In memory Authentication
-
-		// http.authorizeRequests().antMatchers("/activity/**").access("hasRole('ROLE_ADMIN')
-		// or hasRole('ROLE_USER')")
-		// .antMatchers("/info/**").access("hasRole('ROLE_ADMIN') or
-		// hasRole('ROLE_MANAGER')").and().formLogin();
-
-		// 2. jdbc authentication
-
 		http.authorizeRequests().antMatchers("/registration/**", "/login/**").permitAll().antMatchers("/team/**")
 				.access("hasRole('ADMIN') or hasRole('USER')").antMatchers("/player/**")
 				.access("hasRole('ADMIN') or hasRole('USER')").anyRequest().authenticated().and().csrf().disable()
 				.httpBasic();
-
 	}
 
 }
