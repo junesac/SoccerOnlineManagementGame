bc13eae9833edbe0bc80ee00b614827a582cbf34
Sachin Kumar <sjun1992@gmail.com>
2018-06-28 22:33:24 +0530

    Fixed logout functionality

--- src/main/webapp/resources/js/controllers/LogoutController.js
+++ src/main/webapp/resources/js/controllers/LogoutController.js
@@ -6,13 +6,16 @@ app.controller('LogoutController', function($rootScope, CommonService, $scope,
 		$location) {
 	$scope.logout = function() {
 		$rootScope.authenticated = false;
-		CommonService.auth = '';
 		$rootScope.admin = false;
 		$rootScope.user = false;
+		$rootScope.owner = '' ;
+
 		CommonService.countries = [];
 		CommonService.playerTypes = [];
 		CommonService.team = {};
 		CommonService.users = [];
+		CommonService.auth = '';
+
 		$location.path('/login');
 	}
 	$scope.logout();
